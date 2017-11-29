3ce5757e61dad7556c58a5e36e6ed1ae647c97f2
Nat Wright <natalee.wright413@gmail.com>
2017-11-29 11:51:50 -0700

    EnemyScript

---
+++ FinalGame/Assets/Scripts/AI/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	public Sprite[] enemySprite;
+	public SpriteRenderer myEnemy;
+
+	while (player = !gameOver)
+	{
+
+	}
+}
