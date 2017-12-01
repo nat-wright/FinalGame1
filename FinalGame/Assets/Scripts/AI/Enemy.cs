c24d44a98e2c54f5efaf1f9daf21b9e59e60a5c0
Nat Wright <natalee.wright413@gmail.com>
2017-12-01 11:51:23 -0700

    EnemyScriptArray

--- FinalGame/Assets/Scripts/AI/Enemy.cs
+++ FinalGame/Assets/Scripts/AI/Enemy.cs
@@ -1,14 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Enemy : MonoBehaviour {
 
-	public Sprite[] enemySprite;
+	public Sprite[] enemySprite = new Sprite[3];
 	public SpriteRenderer myEnemy;
+	public Image bar;
+	
 
-	while (player = !gameOver)
-	{
-
+	public void Start() {
+		StartCoroutine (enemySwitch());
 	}
+
+
+		IEnumerator enemySwitch () 
+	{
+		
+		while (bar.fillAmount > 0)
+		{
+			for (int i = 0;  i < enemySprite.Length; i++)
+			{
+				myEnemy.sprite = enemySprite[i];
+				yield return new WaitForSeconds(0.1f);
+			}
+			
+		}
+	}
+	
 }
