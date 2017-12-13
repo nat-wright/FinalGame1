ebe7812d28002b30e00f039792436c00a6a1befa
Nat Wright <natalee.wright413@gmail.com>
2017-12-13 11:50:12 -0700

    AddedKeyScript

---
+++ FinalGame/Assets/Scripts/UI/Key.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Key : MonoBehaviour {
+
+	public GameObject winnerText;
+	public Image bar;
+	
+//When collided with key, game will end (needs fixing)
+	public void OnTriggerEnter () 
+	{
+		  if (bar.fillAmount > 0)
+        {
+            winnerText.SetActive(true);
+            CharacterControl.gameOver = true;
+        }
+	}
+}	
